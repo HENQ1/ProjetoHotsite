639be50c93af02d5e214e22d5dabdb53e3579b69
HENQ1 <henriqueschommer1712@gmail.com>
2023-10-30 20:03:50 -0300

    Criação do projeto

---
+++ wwwroot/js/site.js
@@ -0,0 +1,51 @@
+﻿
+$(document).ready(
+
+    function () {
+        $("#formCadastro").submit(
+            function (e) {
+
+               e.preventDefault();
+                cadastrar();
+
+            }
+
+        );
+
+    }
+);
+
+function cadastrar() {
+    let parametros = {
+        Nome: $("#idNome").val(),
+        Email: $("#idEmail").val(),
+        Mensagem: $("#idMensagem").val(),
+
+    };
+
+    $.post("/Home/Cadastrar", parametros)
+
+        .done(function (data) {
+
+            if (data.status == "ok") {
+
+                $("#formCadastro").after("<h3>Cadastro realizado com sucesso!</h3>");
+                $("#formCadastro").hide();
+            }
+            else {
+                $("#formCadastro").after("<h3> falha no Cadastro !</h3>");
+                $("#formCadastro").hide();
+
+            }
+        }
+
+        )
+    fail(function () {
+        alert("Erro");
+
+    }
+    )
+};
+
+
+
